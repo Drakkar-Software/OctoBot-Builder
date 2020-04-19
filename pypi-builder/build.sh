1108fe301c074229ad4c6d288cd50651e051c2b0
Herklos <herklos.dev@protonmail.com>
2020-04-19 09:47:27 +0200

    Add multiple arch
    Signed-off-by: Herklos <herklos.dev@protonmail.com>

--- pypi-builder/build.sh
+++ pypi-builder/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /project
-python -m pip install --only-binary cryptography auditwheel cryptography twine
+python -m pip install --prefer-binary auditwheel cryptography twine
 python -m pip install --prefer-binary -r dev_requirements.txt -r requirements.txt
 python setup.py bdist_wheel
 auditwheel repair /project/$(find dist -iname "*.whl")
