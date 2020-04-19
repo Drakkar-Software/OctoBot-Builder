91ccdac60ac1f76e9e78c02481a580e3472245ea
Herklos <herklos.dev@protonmail.com>
2020-04-19 14:57:01 +0200

    Refactor project folders

--- pypi-builder/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /project
-python -m pip install --prefer-binary auditwheel cryptography twine
-python -m pip install --prefer-binary -r dev_requirements.txt -r requirements.txt
-python setup.py bdist_wheel
-auditwheel repair /project/$(find dist -iname "*.whl")
-python -m twine upload /project/$(find wheelhouse -iname "*.whl") -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing
-rm -rf /project/build /project/wheelhouse /project/dist
