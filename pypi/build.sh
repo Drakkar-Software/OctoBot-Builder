9b2c0ec414c18c9dc8f27250a83c22c958ad1db0
Herklos <herklos.dev@protonmail.com>
2020-04-19 16:20:35 +0200

    Add aarch64 wheels
    Signed-off-by: Herklos <herklos.dev@protonmail.com>

--- pypi/build.sh
+++ pypi/build.sh
@@ -3,11 +3,11 @@ cd /project
 ${PYTHON_PATH}/python -m pip install --prefer-binary auditwheel cryptography twine
 ${PYTHON_PATH}/python -m pip install --prefer-binary -r dev_requirements.txt -r requirements.txt
 
-if [[ "$BUILD_TYPE" = "WHEEL" ]]; then
+if [ "$BUILD_TYPE" = "WHEEL" ]; then
     ${PYTHON_PATH}/python setup.py bdist_wheel
     auditwheel repair $(find dist -iname "*.whl")
-elif [[ "$BUILD_TYPE" = "SDIST" ]]; then
+     ${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing $(find wheelhouse -iname "*.whl")
+elif [ "$BUILD_TYPE" = "SDIST" ]; then
     ${PYTHON_PATH}/python setup.py sdist
+    ${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing $(find dist -iname "*.tar.gz")
 fi
-
-${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing dist/*
