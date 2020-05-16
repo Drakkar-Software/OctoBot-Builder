3200345119302502ebe0e1e01a0e6ddb9920fa63
herklos <herklos.dev@protonmail.com>
2020-05-16 17:58:29 +0200

    Fix pypi builder

--- pypi/build.sh
+++ pypi/build.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-cd /project
+cd /project || exit
 ${PYTHON_PATH}/python -m pip install --prefer-binary auditwheel cryptography twine
 ${PYTHON_PATH}/python -m pip install --prefer-binary -r dev_requirements.txt -r requirements.txt
 
+# clean project
+make clean
+
+# build and publish
 if [ "$BUILD_TYPE" = "WHEEL" ]; then
     ${PYTHON_PATH}/python setup.py bdist_wheel
-    auditwheel repair $(find dist -iname "*.whl")
-     ${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing $(find wheelhouse -iname "*.whl")
+    auditwheel repair dist/*
+    ${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing wheelhouse/*
 elif [ "$BUILD_TYPE" = "SDIST" ]; then
     ${PYTHON_PATH}/python setup.py sdist
-    ${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing $(find dist -iname "*.tar.gz")
+    ${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing dist/*
 elif [ "$BUILD_TYPE" = "STDEB" ]; then
     ${PYTHON_PATH}/python setup.py --command-packages=stdeb.command bdist_deb
-    ${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing $(find deb_dist -iname "*.deb")
+    ${PYTHON_PATH}/python -m twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing deb_dist/*
 fi
